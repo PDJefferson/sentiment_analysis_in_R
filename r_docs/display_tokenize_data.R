46b4fc1c5dc4a2f81211d81d00aec0c323c9af79
PDJefferson <djefferson545@gmail.com>
2022-03-09 17:54:31 -0800

    created function to display the frequency of sentiments in songs. closes #16

---
+++ r_docs/display_tokenize_data.R
@@ -0,0 +1,25 @@
+create_graph_to_display_frequency_of_sentiments <- function(dataset) {
+  
+  #separating each word from a single row/lyric while
+  #keeping track of the lyric
+  tokenized_dataset <- dataset %>%
+    group_by(artist) %>%
+    mutate(linenumber = row_number()) %>%
+    ungroup() %>%
+    unnest_tokens(word, lyric)
+    
+  
+  #we get the words that have in common with bing and use the linen number to 
+  #to keep track of the song. we separate the filling positive and negative by
+  #pivot wider and finally we get the net sentiment of the lyric
+  prepare_tokenized_dataset <- tokenized_dataset %>%
+    inner_join(get_sentiments("bing")) %>%
+    count(artist, song_number = linenumber %/% 4, sentiment) %>%
+    pivot_wider(names_from = sentiment, values_from = n, values_fill = 0) %>%
+    mutate(sentiment = positive - negative)
+  
+  ggplot(prepare_tokenized_dataset, aes(song_number, sentiment, fill= artist)) +
+    geom_col(show.legend = FALSE) +
+    facet_wrap(~artist, ncol = 4, scales= "free_x")
+  
+}
